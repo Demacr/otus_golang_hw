7f5ffed67eb8b557afa1a35b005bfec9ce5e4dd3
Daniil Karandashov <karandashovd@gmail.com>
2020-05-28 21:31:28 +0300

    hw01 completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	logger := log.New(os.Stderr, "", 0)
+
+	curTime := time.Now()
+	exTime, err := ntp.Time("0.ru.pool.ntp.org")
+	if err != nil {
+		logger.Fatal("Error: can't get network time: ", err)
+	}
+
+	fmt.Println("current time:", curTime.Round(time.Second))
+	fmt.Println("exact time:", exTime.Round(time.Second))
 }
