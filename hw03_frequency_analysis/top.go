d239e24833ad2da6cf87de0999de4c10269685e4
Daniil Karandashov <karandashovd@gmail.com>
2020-06-17 14:42:06 +0300

    Hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+// Top10 returns top 10 words in the text
+//
+func Top10(text string) []string {
+	// Using map [string]int to count lowercased words in the text
+	wordStats := map[string]int{}
+	words := strings.FieldsFunc(text, func(c rune) bool {
+		return unicode.IsSpace(c) || c == '–' || c == '—'
+	})
+	for _, word := range words {
+		// Trimming
+		_word := strings.Trim(word, "?.,!-")
+		wordStats[strings.ToLower(_word)]++
+	}
+	delete(wordStats, "")
+
+	// Using slice of string and sort by map count value
+	wordStatsSlice := make([]string, 0, len(wordStats))
+	for word := range wordStats {
+		wordStatsSlice = append(wordStatsSlice, word)
+	}
+	sort.Slice(wordStatsSlice, func(i, j int) bool {
+		return wordStats[wordStatsSlice[i]] > wordStats[wordStatsSlice[j]]
+	})
+
+	// return slice as is if length less or equal 10
+	if len(wordStatsSlice) <= 10 {
+		return wordStatsSlice
+	}
+	return wordStatsSlice[:10]
 }
