2f31e8b2fee07ab887ed41f20dcecae04596e0ac
Daniil Karandashov <karandashovd@gmail.com>
2020-08-08 21:48:09 +0300

    hw08

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	// Place your code here
+	args := os.Args
+	if len(args) < 3 {
+		panic("too few arguments")
+	}
+
+	dir, err := os.Stat(args[1])
+	if err != nil {
+		panic(err)
+	}
+	if !dir.IsDir() {
+		panic("First argument is not a dir")
+	}
+
+	env, err := ReadDir(args[1])
+	if err != nil {
+		panic(err)
+	}
+	rc := RunCmd(args[2:], env)
+
+	os.Exit(rc)
 }
